73e4412b7b4c1288a4358cae191d41e777802ebc
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2023-04-09 16:48:08 -0300

    la

--- srcs/requirements/wordpress/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Faustoooow</h1>
-
---
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "/var/www/wordpress/wp-config.php" ]; then
+    wp core download --allow-root
+
+    wp core config --dbhost="${MARIADB_ROOT_HOST}" --dbname="${MARIADB_DATABASE}" --dbuser="${MARIADB_USER}" --dbpass="${MARIADB_PASSWORD}" --allow-root
+
+    wp core install --url="${DOMAIN_NAME}" --title="${DOMAIN_NAME}" --admin_name="${WP_ADM_USR}" --admin_password="${WP_ADM_PSW}" --admin_email="${WP_ADM_MAIL}" --allow-root
+
+fi
+
+php-fpm7.3 -F
+
