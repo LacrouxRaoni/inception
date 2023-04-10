fecc8b246fa7433b6ec33571bb8dc683ad8f2d06
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2023-04-10 16:34:53 -0300

    almost there

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ WP_VOLUME		=	$(DATA_PATH)/wp/
 all:
 	@ sudo mkdir -p $(DB_VOLUME)
 	@ sudo mkdir -p $(WP_VOLUME)
+	@ sudo sed -i "s/127.0.0.1	localhost/127.0.0.1 rruiz-la.42.fr/" /etc/hosts
 	@ docker-compose -f $(DOCKER_COMPOSE) up --build -d
 
 clean: stop
--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -6,6 +6,8 @@ if [ ! -d "/var/www/wordpress/wp-config.php" ]; then
     wp core config --dbhost="${MARIADB_ROOT_HOST}" --dbname="${MARIADB_DATABASE}" --dbuser="${MARIADB_USER}" --dbpass="${MARIADB_PASSWORD}" --allow-root
 
     wp core install --url="${DOMAIN_NAME}" --title="${DOMAIN_NAME}" --admin_name="${WP_ADM_USR}" --admin_password="${WP_ADM_PSW}" --admin_email="${WP_ADM_MAIL}" --allow-root
+    
+    wp user create "${WP_USR}" "${WP_USR_MAIL}" --user_pass="${WP_PSW}" --porcelain --allow-root
 
 fi
 
